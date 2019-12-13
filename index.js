b9420890a8d320af561a7c9523e179266e78ff2f
Berat Bozkurt <beratbozkurt1999@gmail.com>
2019-12-13 12:27:51 +0300

    express eklendi

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
+const express = require('express');
 const config = require('./config');
 const twiti = require('twit');
-const twit = new twiti(config);
 
-function retweet() {
-    let params = {
-        q: 'evleniyorum',
-        count: 10,
-        result_type: 'recent',
-    }
-    twit.get('search/tweets', params, (err, data, response) => {
-        let tweets = data.statuses
-        console.log(data)
-        var sayac = 0;
-        for (let dat of tweets) {
-            let username = dat.user.screen_name
-            let tweetID = dat.id_str
-            let reply = dat.in_reply_to_status_id_str
-            if (!err && reply === null && sayac === 0) {
-                twit.post('statuses/update', {
-                    status: `@${username} Daha önce dijital davetiye oluşturmayı denedin mi? Hemen incelemek için aşağıdaki linke tıkla: davetiyem.co/damatgelin`,
-                    in_reply_to_status_id: tweetID
-                }, function (err, data, response) {
-                    if (err) {
-                        console.log("error")
-                    } else {
-                        sayac = 1;
-                        twit.post('favorites/create', {
-                            id: tweetID
-                        }, function (errorr, dataa, responsee) {
-                            if (errorr) {
-                                console.log('CANNOT BE FAVORITE... Error');
-                            }
-                            else {
-                                console.log('FAVORITED... Success!!!');
-                            }
-                        })
-                        console.log(username + ' tweeted!')
-                    }
-                })
-            }
+const twit = new twiti(config);
+const app = express();
+
+
+app.get('/', (req, res) => {
+
+    function retweet() {
+        let params = {
+            q: 'evleniyorum',
+            count: 10,
+            result_type: 'recent',
         }
-    })
-}
-setInterval(retweet, 900000)
+        twit.get('search/tweets', params, (err, data, response) => {
+            let tweets = data.statuses
+            console.log(data)
+            var sayac = 0;
+            for (let dat of tweets) {
+                let username = dat.user.screen_name
+                let tweetID = dat.id_str
+                let reply = dat.in_reply_to_status_id_str
+                if (!err && reply === null && sayac === 0) {
+                    twit.post('statuses/update', {
+                        status: `@${username} Daha önce dijital davetiye oluşturmayı denedin mi? Hemen incelemek için aşağıdaki linke tıkla: davetiyem.co/damatgelin`,
+                        in_reply_to_status_id: tweetID
+                    }, function (err, data, response) {
+                        if (err) {
+                            console.log("error")
+                        } else {
+                            sayac = 1;
+                            twit.post('favorites/create', {
+                                id: tweetID
+                            }, function (errorr, dataa, responsee) {
+                                if (errorr) {
+                                    console.log('CANNOT BE FAVORITE... Error');
+                                }
+                                else {
+                                    console.log('FAVORITED... Success!!!');
+                                }
+                            })
+                            console.log(username + ' tweeted!')
+                        }
+                    })
+                }
+            }
+        })
+    }
+    setInterval(retweet, 900000)
+
+
+})
+
+app.listen(process.env.PORT || 3000, () => console.log("server ayakta"))
