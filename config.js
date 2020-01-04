c6e1b22885ee021dffef5f1a4758a3b41d42d7b6
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-01-05 00:18:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:'kEsfqspHuzDf5lMKaCiQAsEzu',  
-    consumer_secret:'WRSweQVE9YwcbUYJRXkb7uy95DYiepvWoSFTrhQoX65btjDUc8',
-    access_token:'743725401378152448-1gjn1J6wMRNZ6kzAU0CDKNuswS6LgYy',  
-    access_token_secret:'0ae1WtIPZcFFaui70Fmxc1dnxWcAgW3THkW3CsNxp6Fxn'
+    consumer_key:'XXXX',  
+    consumer_secret:'XXXXXXXX',
+    access_token:'XXXX-XXXXX',  
+    access_token_secret:'XXXXX'
 }
